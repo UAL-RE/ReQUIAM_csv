57aacfb64c52df274581e4fffe3bbeece73db33d
Chun Ly <astro.chun@gmail.com>
2020-02-17 13:31:57 -0700

    Add setup.py for package installation (iss #12, #8)

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='DataRepository_research_themes',
+    version='v0.3.0',
+    packages=find_packages('DataRepository_research_themes'),
+    url='https://github.com/ualibraries/DataRepository_research_themes',
+    license='MIT License',
+    author='Chun Ly',
+    author_email='astro.chun@gmail.com',
+    description='Construct list of research themes and organization mapping to work with figshare patron management',
+    long_description=long_description,
+    long_description_content_type='text/markdown'    
+)
