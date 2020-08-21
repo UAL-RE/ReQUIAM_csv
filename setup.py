c6015df7a1dcb51ad833151c1d7cc0dd6e495aba
Chun Ly <astro.chun@gmail.com>
2020-08-20 17:02:28 -0700

    Update setup.py for pypi compatibility #46

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as fr:
 setup(
     name='requiam_csv',
     version='0.10.2',
-    packages=find_packages('requiam_csv'),
+    packages=find_packages(),
     url='https://github.com/ualibraries/ReQUIAM_csv',
     license='MIT License',
     author='Chun Ly',
@@ -17,5 +17,6 @@ setup(
     description='Construct list of research themes and organization mapping to work with figshare patron management',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=requirements
+    install_requires=requirements,
+    include_package_data=True
 )
