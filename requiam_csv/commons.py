be7538ee77398241c390b9ed7642a90ca648af7a
Chun Ly <astro.chun@gmail.com>
2021-02-16 10:02:13 -0700

    Type hinting for commons module

--- requiam_csv/commons.py
+++ requiam_csv/commons.py
@@ -1,7 +1,8 @@
 import numpy as np
+from pandas import DataFrame
 
 
-def no_org_code_index(df):
+def no_org_code_index(df: DataFrame) -> np.ndarray:
     """
     Purpose:
       Identify entries without an Org Code.  This is based on whether
