a04b1591e7ca9315cec6525bfd7bdc7a76d00481
Chun Ly <astro.chun@gmail.com>
2021-07-01 13:18:34 -0700

    Fix copyright in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'ReQUIAM_csv'
-copyright = '2021, University of Arizona Libraries'
+copyright = '2021, Arizona Board of Regents'
 author = 'Chun Ly'
 
 # The full version, including alpha/beta/rc tags
